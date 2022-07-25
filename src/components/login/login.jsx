3e3fa019e520183565092815fb3cc9cf68920214
junvely <junvely97@gmail.com>
2022-07-26 01:42:46 +0900

    :sparkles: Create : Firebase 사용해보기, Firebase를 이용한 Google 로그인 기능 적용

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import styles from "./login.module.css";
+import { auth } from "../../firebase";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useState } from "react";
 
 const Login = (props) => {
+  const [userData, setUserData] = useState(null);
+
+  function handleGoogleLogin() {
+    const provider = new GoogleAuthProvider(); // provider를 구글로 설정
+    signInWithPopup(auth, provider) // popup을 이용한 signin
+      .then((data) => {
+        setUserData(data.user); // user data 설정
+        console.log(data); // console로 들어온 데이터 표시
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   return (
     <div className={styles.loginCon}>
       <div className={styles.imgCon}>
@@ -15,7 +32,11 @@ const Login = (props) => {
         <h2 className={styles.title}>CARD MAKER</h2>
         <p className={styles.info}>Login and get started!</p>
         <div className={styles.buttons}>
-          <button className={styles.button}>Goggle 로그인</button>
+          {/* 적용 */}
+          <button className={styles.button} onClick={handleGoogleLogin}>
+            Goggle 로그인
+          </button>
+          {userData ? userData.displayName : null}
           <button className={styles.button}>Face Book 로그인</button>
         </div>
       </div>
