0b8014565cefa647e25b7942105744f7807f4529
junvely <junvely97@gmail.com>
2022-08-12 23:53:36 +0900

    :recycle: Refactoring : 이미지 업로드(2)-Error 수정 : Cloudinary에 사진 업로드 후, url과 upload_preset 수정 : 이미지 업로드 완료

--- src/service/image_uploader.js
+++ src/service/image_uploader.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 
+const url = "https://api.cloudinary.com/v1_1/diucr969q/image/upload";
+
 class ImageUploader extends Component {
   async upload(file) {
     // file을 받아 file을 업로드, 업로드 성공 시 서버에게 url 전달
     const data = new FormData();
     data.append("file", file);
-    data.append("upload_preset", "docs_upload_example_us_preset"); // post에 필요한 데이터
+    data.append("upload_preset", "evdlglut"); // post에 필요한 데이터
     // 완료되면 result를 받아서
     const result = await fetch(
-      "https://res.cloudinary.com/diucr969q/image/upload/v1658390628/cld-sample-5.jpg", // cloudinary 이미지 url 주소 전송
+      url, // cloudinary 이미지 url 주소 전송
       {
         method: "POST",
         body: data,
